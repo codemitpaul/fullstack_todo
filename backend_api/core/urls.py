d37b1f858650b22c0159e1b823fbcdf50571cc8e
Paul Mill <pauloprogramming@web.de>
2022-12-11 21:23:49 +0100

    first

---
+++ backend_api/core/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, include
+from . import views 
+
+urlpatterns = [
+    path('', views.home, name="home"),
+    path('todos/', views.todos_list, name="todos_list"),
+    path('todos/<str:pk>/', views.todo, name='todo')
+]
